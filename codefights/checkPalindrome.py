015e5c78e518bb2852399f4f27af16c91bfd8e7a
maxvalrus <maxval.rus@gmail.com>
2018-05-29 23:32:35 +0300

    codefights

--- codefights/checkPalindrome.py
+++ codefights/checkPalindrome.py
@@ -2,11 +2,11 @@ def checkPalindrome(inputString):
     i = 0
     max = len(inputString) // 2
     while i <= max:
-        print(inputString[i], inputString[-i - 1])
         if inputString[i] == inputString[-i - 1]:
             i += 1
         else:
             return False
     return True
 
-checkPalindrome('aabaa')
+if __name__ == "__main__":
+    print(checkPalindrome('aabaa'))
