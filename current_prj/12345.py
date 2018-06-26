96214505b91767ffcd1c0bc8f08a1d43d8989e38
maxvalrus <maxval.rus@gmail.com>
2018-06-27 01:04:59 +0300

    json

--- current_prj/12345.py
+++
@@ -1,9 +0,0 @@
-def fib():
-    a, b = 0, 1
-    while True:
-        yield a
-        a, b = b, a + b
-
-x = fib()
-for i in range(0, 10):
-    print(next(x))
---
+++ lesson9/json.py
@@ -0,0 +1,22 @@
+import json
+
+def read_file(file):
+    with open(file) as f:
+        return f.read()
+
+def write_file(file, text, mode='w'):
+    '''
+    mode:
+    w - all write files
+    a - add string
+    '''
+    with open(file, mode=mode) as f:
+        f.write(text)
+
+if __name__ == "__main__":
+    data = read_file("..\lesson9\data.json")
+    print(data)
+    obj = json.loads(data)
+    print(obj["object"]["key"][0])
+    obj["new"] = "secret"
+    print(json.dumps(obj, sort_keys=True, indent=4))
