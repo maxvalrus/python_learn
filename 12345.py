cc994a3416c9f3971ec969c20af5a0d7ff658307
maxvalrus <maxval.rus@gmail.com>
2018-09-09 12:40:32 +0300

    flask_habr

--- 12345.py
+++ 12345.py
@@ -2,14 +2,17 @@ from flask import Flask, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, validators, IntegerField
 
+
 class ContactForm(FlaskForm):
     name = StringField(label='Name', validators=[validators.Length(min=4, max=25)])
     email = StringField(label='E-mail', validators=[validators.Length(min=4, max=35), validators.Email()])
     job = StringField(validators=[validators.Required(), validators.AnyOf(("IT", "Bank", "HR"))])
     month = IntegerField(validators=[validators.data_required(), validators.NumberRange(min=1, max=12)])
 
+
 app = Flask(__name__)
-app.config.update(DEBUG=True, SECRET_KEY='This key must be secret!', WTF_CSRF_ENABLED=False)
+# app.config.update(DEBUG=True, SECRET_KEY='This key must be secret!', WTF_CSRF_ENABLED=False)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -18,14 +21,15 @@ def home():
 
         if form.validate():
             if int(request.form['month']) == 5:
-                return ('Cool!', 200)
+                return 'Cool!', 200
             else:
-                return ('Month != 5', 400)
+                return 'Month != 5', 400
         else:
-            return ('invalid', 400)
+            return 'invalid', 400
 
     if request.method == 'GET':
         return 'hello world!', 200
+
 
 if __name__ == '__main__':
     app.run()
---
+++ app_mod/routes.py
@@ -0,0 +1,22 @@
+from app_mod import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Эльдар Рязанов'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
